a8cc3b0c5b28b8a5057611212a1585632f21cabf
Kristy Keller <keller.kristy@outlook.com>
2020-09-10 14:53:51 -0400

    pseudo code added

---
+++ JavaScript.js
@@ -0,0 +1,18 @@
+//get current day link to inner HTML to show on top of screen
+
+
+// color code timeblocks for hours; look into for loops per time
+
+
+// class with .past, .present, .future 
+
+
+// make save button work via onclick 
+// save to local storage as apart of save button function
+
+
+// find a way to load local storage to add to html 
+
+
+// use localStorage.getItem, put text into HTML (make sure javascript.js always does this via a function, make sure you call it)
+
