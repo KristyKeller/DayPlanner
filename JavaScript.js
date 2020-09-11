2ad164d2eb40b6e17fc730dbecb799e36443debf
Kristy Keller <keller.kristy@outlook.com>
2020-09-11 01:04:15 -0400

    on click added to save button

--- JavaScript.js
+++ JavaScript.js
@@ -5,12 +5,18 @@ $("#currentDay").text(today);
 
 // color code time blocks for hours; look into for loops per time
 
+moment().format('LT'); 
+
 
 // class with .past, .present, .future 
 
 
-// make save button work via onclick 
+// make save button works via onclick 
 // save to local storage as apart of save button function
+
+$(".saveBtn").click(function() {
+    console.log("save");
+});
 
 
 // find a way to load local storage to add to html 
