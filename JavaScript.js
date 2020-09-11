52d120bd74917f827453e0915cbcef415984f015
Kristy Keller <keller.kristy@outlook.com>
2020-09-10 22:02:16 -0400

    start button added

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,6 @@
-//get current day link to inner HTML to show on top of screen
-currentDay 
-moment().format('llll');
+// //get current day link to inner HTML to show on top of screen
+// id="currentDay" class="lead"> 
+// moment().format('LLLL');
 
 // color code time blocks for hours; look into for loops per time
 
